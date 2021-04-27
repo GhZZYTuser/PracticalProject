83cd1fc6f4dd54d07f75e25fab896433547caf7a
GhZZYTuser <45088146+GhZZYTuser@users.noreply.github.com>
2021-04-27 18:31:47 +0800

    feat-Book

---
+++ bean/Book.java
@@ -0,0 +1,57 @@
+package worker.bean;
+
+public class Book {
+    private Integer id;
+
+    private String name;
+
+    private Double price;
+
+    private String publisher;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", price=").append(price);
+        sb.append(", publisher=").append(publisher);
+        sb.append("]");
+        return sb.toString();
+    }
+}
