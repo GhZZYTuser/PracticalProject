9b5645f989d19d84f120b3caa42e002652649a14
17585194504 <83221465+17585194504@users.noreply.github.com>
2021-04-27 19:02:54 +0800

    fit：service_1

--- src/main/java/worker/service/BookService.java
+++ src/main/java/worker/service/BookService.java
@@ -5,3 +5,17 @@ import worker.bean.Book;
 import java.util.List;
 
 
+public interface BookService{
+
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Book record);
+
+    Book selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKey(Book record);
+
+    List<Book>  selectAll();
+
+}
